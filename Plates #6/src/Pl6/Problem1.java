8ee627178ec737a4c17c718f62dbee1c5b1e76bf
mavmont <98688317+mavmont@users.noreply.github.com>
2024-05-30 21:54:10 +0800

    Update Problem1.java

--- Plates #6/src/Pl6/Problem1.java
+++ Plates #6/src/Pl6/Problem1.java
@@ -1,8 +1,6 @@
 package Pl6;
 import java.util.*;
 
-import java.util.*;
-
 public class Problem1 { // Write a java program that receives a list of edges of a simple graph, the program should determine whether it is connected
                        // and find the number of connected components if it is not connected.
     private int numVertices;
