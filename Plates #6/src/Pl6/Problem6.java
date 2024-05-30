30a0b3d5729aeebffe10ce0ec10181ffacbf2d60
mavmont <98688317+mavmont@users.noreply.github.com>
2024-05-31 04:23:31 +0800

    Update Problem6.java

--- Plates #6/src/Pl6/Problem6.java
+++ Plates #6/src/Pl6/Problem6.java
@@ -1,11 +1,10 @@
 package Pl6;
-import java.util.*;
+import java.util.Scanner;
 
 public class Problem6 {
     private int numVertices;
     private int[][] adjacencyMatrix;
 
-   
     public Problem6(int numVertices) {
         this.numVertices = numVertices;
         adjacencyMatrix = new int[numVertices][numVertices];
@@ -26,72 +25,23 @@ public class Problem6 {
     }
 
     public static void main(String[] args) {
-        int numVertices1 = 5;
-        Problem6 graph1 = new Problem6(numVertices1);
-        int[][] edges1 = {
-            {0, 1},
-            {0, 2},
-            {1, 2},
-            {2, 0},
-            {2, 2}, 
-            {3, 4},
-            {4, 3}, 
-            {3, 4}  
-        };
-        for (int[] edge : edges1) {
-            graph1.addEdge(edge[0], edge[1]);
-        }
-        System.out.println("Test Case 1:");
-        graph1.printAdjacencyMatrix();
-        System.out.println();
-        
-        int numVertices2 = 4;
-        Problem6 graph2 = new Problem6(numVertices2);
-        int[][] edges2 = {
-            {0, 1},
-            {1, 2},
-            {2, 3}
-        };
-        for (int[] edge : edges2) {
-            graph2.addEdge(edge[0], edge[1]);
-        }
-        System.out.println("Test Case 2:");
-        graph2.printAdjacencyMatrix();
-        System.out.println();
+        Scanner scanner = new Scanner(System.in);
 
-        int numVertices3 = 3;
-        Problem6 graph3 = new Problem6(numVertices3);
-        int[][] edges3 = {
-            {0, 0},
-            {1, 1},
-            {2, 2}
-        };
-        for (int[] edge : edges3) {
-            graph3.addEdge(edge[0], edge[1]);
-        }
-        System.out.println("Test Case 3:");
-        graph3.printAdjacencyMatrix();
-        System.out.println();
+        System.out.print("Enter the number of vertices: ");
+        int numVertices = scanner.nextInt();
 
-        int numVertices4 = 3;
-        Problem6 graph4 = new Problem6(numVertices4);
-        int[][] edges4 = {
-            {0, 1},
-            {0, 1},
-            {1, 2},
-            {1, 2}
-        };
-        for (int[] edge : edges4) {
-            graph4.addEdge(edge[0], edge[1]);
-        }
-        System.out.println("Test Case 4:");
-        graph4.printAdjacencyMatrix();
-        System.out.println();
+        Problem6 graph = new Problem6(numVertices);
 
-        int numVertices5 = 5;
-        Problem6 graph5 = new Problem6(numVertices5);
-        System.out.println("Test Case 5:");
-        graph5.printAdjacencyMatrix();
-        System.out.println();
+        System.out.print("Enter the number of edges: ");
+        int numEdges = scanner.nextInt();
+
+        System.out.println("Enter the edges (format: from to):");
+        for (int i = 0; i < numEdges; i++) {
+            int from = scanner.nextInt();
+            int to = scanner.nextInt();
+            graph.addEdge(from, to);
+        }
+
+        graph.printAdjacencyMatrix();
     }
 }
