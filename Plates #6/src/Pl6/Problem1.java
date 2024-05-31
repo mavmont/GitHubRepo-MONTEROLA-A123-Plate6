2490ddfa73f5fefc2a118695588cdb69ba3be6eb
mavmont <98688317+mavmont@users.noreply.github.com>
2024-05-31 21:51:46 +0800

    Update Problem1.java

--- Plates #6/src/Pl6/Problem1.java
+++ Plates #6/src/Pl6/Problem1.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Problem1 { // Write a java program that receives a list of edges of a simple graph, the program should determine whether it is connected
                        // and find the number of connected components if it is not connected.
-    private int numVertices;
+     private int numVertices;
     private LinkedList<Integer>[] adjacencyList;
 
     public Problem1(int numVertices) {
@@ -14,7 +14,6 @@ public class Problem1 { // Write a java program that receives a list of edges of
         }
     }
 
-    
     void addEdge(int v, int w) {
         adjacencyList[v].add(w);
         adjacencyList[w].add(v); 
@@ -55,44 +54,26 @@ public class Problem1 { // Write a java program that receives a list of edges of
     }
 
     public static void main(String[] args) {
-      
-        Problem1 graph1 = new Problem1(5); //Test Case 1
-        graph1.addEdge(0, 1);
-        graph1.addEdge(0, 2);
-        graph1.addEdge(3, 4);
-        System.out.println("Test Case 1:");
-        printGraphConnectivity(graph1);
+        Scanner scanner = new Scanner(System.in);
 
-        Problem1 graph2 = new Problem1(4); //Test Case 2
-        graph2.addEdge(0, 1);
-        graph2.addEdge(0, 2);
-        graph2.addEdge(0, 3);
-        graph2.addEdge(1, 2);
-        graph2.addEdge(1, 3);
-        graph2.addEdge(2, 3);
-        System.out.println("Test Case 2:");
-        printGraphConnectivity(graph2);
+        System.out.print("Enter the number of vertices in the graph: ");
+        int numVertices = scanner.nextInt();
 
-        Problem1 graph3 = new Problem1(3); //Test Case 3
-        System.out.println("Test Case 3:");
-        printGraphConnectivity(graph3);
+        Problem1 graph = new Problem1(numVertices);
 
-        Problem1 graph4 = new Problem1(6); //Test Case 4
-        graph4.addEdge(0, 1);
-        graph4.addEdge(1, 2);
-        graph4.addEdge(2, 3);
-        graph4.addEdge(3, 4);
-        graph4.addEdge(4, 5);
-        System.out.println("Test Case 4:");
-        printGraphConnectivity(graph4);
+        System.out.print("Enter the number of edges in the graph: ");
+        int numEdges = scanner.nextInt();
 
-        Problem1 graph5 = new Problem1(6); //Test Case 5
-        graph5.addEdge(0, 1);
-        graph5.addEdge(1, 2);
-        graph5.addEdge(3, 4);
-        graph5.addEdge(4, 5);
-        System.out.println("Test Case 5:");
-        printGraphConnectivity(graph5);
+        System.out.println("Enter the edges (vertex pairs): ");
+        for (int i = 0; i < numEdges; i++) {
+            int v = scanner.nextInt();
+            int w = scanner.nextInt();
+            graph.addEdge(v, w);
+        }
+
+        printGraphConnectivity(graph);
+
+        scanner.close();
     }
 
     public static void printGraphConnectivity(Problem1 graph) {
@@ -103,7 +84,5 @@ public class Problem1 { // Write a java program that receives a list of edges of
             System.out.println("The graph is not connected.");
             System.out.println("Number of connected components: " + numComponents);
         }
-        System.out.println();
     }
 }
-
