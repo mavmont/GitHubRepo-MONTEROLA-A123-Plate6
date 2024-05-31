0dc2840c2a31ca35e5370e7b92b52bf0a370ce65
mavmont <98688317+mavmont@users.noreply.github.com>
2024-05-31 22:53:52 +0800

    Update Problem3.java

--- Plates #6/src/Pl6/Problem3.java
+++ Plates #6/src/Pl6/Problem3.java
@@ -3,7 +3,7 @@ package Pl6;
 import java.util.*;
 
 public class Problem3 { // Write a java program that will determine if a graph has a cycle or not.
-    private int numVertices;
+  private int numVertices;
     private LinkedList<Integer>[] adjacencyList;
 
     public Problem3(int numVertices) {
@@ -23,14 +23,11 @@ public class Problem3 { // Write a java program that will determine if a graph h
         visited[v] = true;
 
         for (int n : adjacencyList[v]) {
-            
             if (!visited[n]) {
                 if (isCyclicUtil(n, visited, v)) {
                     return true;
                 }
-            }
-            
-            else if (n != parent) {
+            } else if (n != parent) {
                 return true;
             }
         }
@@ -51,46 +48,24 @@ public class Problem3 { // Write a java program that will determine if a graph h
     }
 
     public static void main(String[] args) {
-        
-        Problem3 graph1 = new Problem3(5); // Test Case 1
-        graph1.addEdge(0, 1);
-        graph1.addEdge(0, 2);
-        graph1.addEdge(1, 2);
-        graph1.addEdge(2, 0);
-        graph1.addEdge(2, 3);
-        graph1.addEdge(3, 4);
-        System.out.println("Test Case 1:");
-        printGraphCycleDetection(graph1);
+        Scanner scanner = new Scanner(System.in);
 
-        Problem3 graph2 = new Problem3(5); // Test Case 2
-        graph2.addEdge(0, 1);
-        graph2.addEdge(1, 2);
-        graph2.addEdge(2, 3);
-        graph2.addEdge(3, 4);
-        System.out.println("Test Case 2:");
-        printGraphCycleDetection(graph2);
+        System.out.print("Enter number of vertices: ");
+        int numVertices = scanner.nextInt();
 
-        Problem3 graph3 = new Problem3(4); // Test Case 3
-        graph3.addEdge(0, 1);
-        graph3.addEdge(0, 2);
-        graph3.addEdge(0, 3);
-        graph3.addEdge(1, 2);
-        graph3.addEdge(1, 3);
-        graph3.addEdge(2, 3);
-        System.out.println("Test Case 3:");
-        printGraphCycleDetection(graph3);
+        Problem3 graph = new Problem3(numVertices);
 
-        Problem3 graph4 = new Problem3(6); // Test Case 4
-        graph4.addEdge(0, 1);
-        graph4.addEdge(1, 2);
-        graph4.addEdge(2, 0);
-        graph4.addEdge(3, 4);
-        System.out.println("Test Case 4:");
-        printGraphCycleDetection(graph4);
+        System.out.print("Enter number of edges: ");
+        int numEdges = scanner.nextInt();
 
-        Problem3 graph5 = new Problem3(3); // Test Case 5
-        System.out.println("Test Case 5:");
-        printGraphCycleDetection(graph5);
+        System.out.println("Enter edges (v1 v2):");
+        for (int i = 0; i < numEdges; i++) {
+            int v1 = scanner.nextInt();
+            int v2 = scanner.nextInt();
+            graph.addEdge(v1, v2);
+        }
+
+        printGraphCycleDetection(graph);
     }
 
     public static void printGraphCycleDetection(Problem3 graph) {
@@ -99,6 +74,5 @@ public class Problem3 { // Write a java program that will determine if a graph h
         } else {
             System.out.println("The graph does not contain a cycle.");
         }
-        System.out.println();
     }
 }
